196aed3405c4e29b3c14b2b37a2720c0e8b7f8c9
nb <nonkovic.bogdana@gmail.com>
2023-02-12 12:48:04 +0400

    feat: add word counting functions

---
+++ src/pycounts_nb/pycounts_nb.py
@@ -0,0 +1,35 @@
+from collections import Counter
+from string import punctuation
+
+def load_text(input_file):
+    """
+    Load text from a text file and return as a string
+
+    Args:
+        input_file (string): Location of the input file
+    """
+    with open(input_file, "r") as file:
+        text = file.read()
+    return text
+
+def clean_text(text):
+    """Lowercase and remove punctuation from a string.
+
+    Args:
+        text (sting): Input strings
+    """
+    text = text.lower()
+    for p in punctuation:
+        text = text.replace(p, "")
+    return text
+
+def count_words(input_file):
+    """Count number of words in input_file
+
+    Args:
+        input_file (string): Location of the input file
+    """
+    text = load_text(input_file)
+    text = clean_text(text)
+    words = text.split()
+    return Counter(words)
